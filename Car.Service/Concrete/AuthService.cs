26276ab0c8896e338b7693b58f80145a77ca5717
Necil <61680106+necilcakmak@users.noreply.github.com>
2021-06-28 15:42:06 +0300

    Update AuthService.cs

--- Car.Service/Concrete/AuthService.cs
+++ Car.Service/Concrete/AuthService.cs
@@ -1,25 +1,19 @@
-﻿using Car.Data.Abstract;
-using Car.Entities.Concrete;
-using Car.Entities.Dtos;
-using Car.Service.Abstract;
-using Car.Shared.Result.Abstract;
-using Car.Shared.Result.Concrete;
-using Car.Shared.Result.Type;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
+using AutoMapper.Configuration;
+using Blog.Business.Abstract;
+using Blog.Data.Abstract;
+using Blog.Entities.Concrete;
+using Blog.Entities.Dto;
+using Blog.Shared.Data.Concrete;
 using System;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Car.Service.Concrete
+namespace Blog.Business.Concrete
 {
     public class AuthService : IAuthService
     {
-
         private readonly IUnitOfWork _unitOfWork;
         private readonly IConfiguration _configuration;
         public AuthService(IUnitOfWork unitOfWork, IConfiguration configuration)
@@ -27,11 +21,9 @@ namespace Car.Service.Concrete
             _unitOfWork = unitOfWork;
             _configuration = configuration;
         }
-        public async Task<IDataResult<CompanyPerson>> Authenticate(LoginDto loginDto)
+        public async Task<Result<Author>> Authenticate(LoginDto loginDto)
         {
-            var loginUser = await _unitOfWork.CompanyPersons.GetAsync(c => c.Email == loginDto.Email);
-            //login isteği atan kullanıcıyı db de ara ilgili email varsa ve parolalar doğru ise gir ve token  üret.
-
+            var loginUser = await _unitOfWork.Authors.GetAsync(c => c.Email == loginDto.Email);
             if (loginUser != null && loginUser.Email == loginDto.Email && BCrypt.Net.BCrypt.Verify(loginDto.Password, loginUser.Password))
             {
                 var tokenHandler = new JwtSecurityTokenHandler();
@@ -52,16 +44,10 @@ namespace Car.Service.Concrete
                 };
                 var token = tokenHandler.CreateToken(tokenDescriptor);
                 //login başarılı ise ilgili kullanıcının bilgilerini ve tokenı dönüyoruz.
-                return new DataResult<CompanyPerson>(ResultStatus.Success, tokenHandler.WriteToken(token), loginUser);
-
+                return new Result<Author>(tokenHandler.WriteToken(token), "Token succesfuly", true);
             }
             //giriş başarısız ise aşağıda ki oluşturduğumuz hatayı dönüyoruz.
-            DataResult<CompanyPerson> res = new DataResult<CompanyPerson>(ResultStatus.Error, "Validation Errors");
-            Dictionary<string, string> validationErrors = new Dictionary<string, string>();
-            validationErrors.Add("Hata", "Email veya Parola Hatalı");
-            res.validationErrors = validationErrors;
-            return res;
-
+            return new Result<Author>(null, "error", false);
         }
     }
 }
