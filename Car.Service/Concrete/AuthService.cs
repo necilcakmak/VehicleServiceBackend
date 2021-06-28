ea721815ab1c93e2946e143f321b3e5594b911e5
Necil <61680106+necilcakmak@users.noreply.github.com>
2021-06-28 15:44:47 +0300

    Update AuthService.cs

--- Car.Service/Concrete/AuthService.cs
+++ Car.Service/Concrete/AuthService.cs
@@ -1,19 +1,25 @@
-using AutoMapper.Configuration;
-using Blog.Business.Abstract;
-using Blog.Data.Abstract;
-using Blog.Entities.Concrete;
-using Blog.Entities.Dto;
-using Blog.Shared.Data.Concrete;
+using Car.Data.Abstract;
+using Car.Entities.Concrete;
+using Car.Entities.Dtos;
+using Car.Service.Abstract;
+using Car.Shared.Result.Abstract;
+using Car.Shared.Result.Concrete;
+using Car.Shared.Result.Type;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Blog.Business.Concrete
+namespace Car.Service.Concrete
 {
     public class AuthService : IAuthService
     {
+
         private readonly IUnitOfWork _unitOfWork;
         private readonly IConfiguration _configuration;
         public AuthService(IUnitOfWork unitOfWork, IConfiguration configuration)
@@ -21,33 +27,28 @@ namespace Blog.Business.Concrete
             _unitOfWork = unitOfWork;
             _configuration = configuration;
         }
-        public async Task<Result<Author>> Authenticate(LoginDto loginDto)
+        public async Task<IDataResult<CompanyPerson>> Authenticate(LoginDto loginDto)
         {
-            var loginUser = await _unitOfWork.Authors.GetAsync(c => c.Email == loginDto.Email);
+            var loginUser = await _unitOfWork.CompanyPersons.GetAsync(c => c.Email == loginDto.Email);
+            //login isteği atan kullanıcıyı db de ara ilgili email varsa ve parolalar doğru ise gir ve token  üret.
+
             if (loginUser != null && loginUser.Email == loginDto.Email && BCrypt.Net.BCrypt.Verify(loginDto.Password, loginUser.Password))
             {
                 var tokenHandler = new JwtSecurityTokenHandler();
-                //appSettings de oluşturduğumuz TokenKey imizi ıConfiguration ile okuyabiliyoruz.
-                var key = Encoding.ASCII.GetBytes(_configuration["TokenKey"]);
-                var tokenDescriptor = new SecurityTokenDescriptor
-                {
-                    Subject = new ClaimsIdentity(new Claim[]
-                    {
-                        new Claim(ClaimTypes.Name,loginUser.Id.ToString()),
-                        new Claim(ClaimTypes.Role,loginUser.Role)
-                    }),
-                    //1 saatlik token ürettik
-                    Expires = DateTime.UtcNow.AddHours(1),
-                    SigningCredentials = new SigningCredentials(
-                        new SymmetricSecurityKey(key),
-                        SecurityAlgorithms.HmacSha256Signature)
+	@@ -52,16 +44,10 @@ public async Task<IDataResult<CompanyPerson>> Authenticate(LoginDto loginDto)
                 };
                 var token = tokenHandler.CreateToken(tokenDescriptor);
                 //login başarılı ise ilgili kullanıcının bilgilerini ve tokenı dönüyoruz.
-                return new Result<Author>(tokenHandler.WriteToken(token), "Token succesfuly", true);
+                return new DataResult<CompanyPerson>(ResultStatus.Success, tokenHandler.WriteToken(token), loginUser);
+
             }
             //giriş başarısız ise aşağıda ki oluşturduğumuz hatayı dönüyoruz.
-            return new Result<Author>(null, "error", false);
+            DataResult<CompanyPerson> res = new DataResult<CompanyPerson>(ResultStatus.Error, "Validation Errors");
+            Dictionary<string, string> validationErrors = new Dictionary<string, string>();
+            validationErrors.Add("Hata", "Email veya Parola Hatalı");
+            res.validationErrors = validationErrors;
+            return res;
+
         }
     }
 }
